28df30c2c6044877c9efec835162ecf110d468ec
bcot-code <78989054+bcot-code@users.noreply.github.com>
2025-11-11 13:29:37 -0600

    init Blog

--- client/src/PAGES/Blog.jsx
+++ client/src/PAGES/Blog.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import {blogs} from "../assets/data"
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className='bg-primary py-16 pt-28'>
+      <div className='max-padd-container'>
+      {/* CONTAINER*/}
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3
+        xl:grid-cols-4 gap-5 gap-y-12'>
+          {blogs.map((blog, index)=>(
+            <div key={index} className='relative'>
+              <div className='bg-white p-4 rounded-2xl'>
+              {/* IMAGE */}
+                <img src={blog.image} alt={blog.title} className='shadow-xl shadow-slate-900/20 rounded-xl'/>
+              </div>
+            {/* INFO */}
+              <p className='text-sm font-semibold mt-6'>{blog.category}</p>
+              <h5 className='pr-4 mb-1 line-clamp-2'>{blog.title}</h5>
+              <p>{blog.description}</p>
+              <button className='underline mt-2 font-bold text-sm
+              line-clamp-2'>continue reading</button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
   )
 }
 
